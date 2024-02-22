c4d0394af48152f5a067fb6f42647b6204bf5627
gustavodev200 <gustavolage200@gmail.com>
2024-02-22 16:07:49 -0300

    feat: added login page

--- src/store/useAuthenticatedStore.ts
+++ src/store/useAuthenticatedStore.ts
@@ -1,35 +1,29 @@
 import create from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
 
 type State = {
-  id?: string;
   name: string;
-  date?: Date;
-  isComplete?: boolean;
-  content?: string;
 };
 
 type Action = {
   updateName: (name: State["name"]) => void;
-  updateDate: (date: Date) => void;
-  updateIsComplete: (isComplete: boolean) => void;
-  updateContent: (content: State["content"]) => void;
 };
 
-export const useAuthenticatedStore = create(
-  persist(
-    (set, get) => ({
-      id: "",
-      name: "",
-      date: "",
-      isComplete: false,
-      content: "",
+export const useAuthenticatedStore = create<State & Action>((set) => ({
+  name: "",
+  updateName: (name) => set(() => ({ name: name })),
+}));
 
-      updateName: () => set({ name: get() }),
-    }),
-    {
-      name: "@userTask", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
-    }
-  )
-);
+// persist(
+//     (set, get) => ({
+//       id: "",
+//       name: "",
+//       date: "",
+//       isComplete: false,
+//       content: "",
+
+//       updateName: () => set({ name: get() }),
+//     }),
+//     {
+//       name: "@userTask", // name of the item in the storage (must be unique)
+//       storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+//     }
